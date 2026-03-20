622b875bbeb1c34a3d188520d60b26ee10021d40
Antekkkkk <antek.kukowski1@gmail.com>
2026-03-20 14:48:41 +0100

    auto uzupelnianie bazy online

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    // Add the Google services Gradle plugin
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -56,4 +58,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    // Import the Firebase BoM
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
 }
--- app/src/main/java/com/example/projekt1/GameViewModel.kt
+++ app/src/main/java/com/example/projekt1/GameViewModel.kt
@@ -1,8 +1,31 @@
 package com.example.projekt1
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class GameViewModel : ViewModel() {
+    companion object {
+        private const val TAG = "GameViewModel"
+    }
+
+    init {
+        val db = Firebase.firestore
+        val docRef = db.collection("game").document("ifEs8o9KugHUmURSGScJ")
+        docRef.addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                Log.d(TAG, "Current data: ${snapshot.data}")
+            } else {
+                Log.d(TAG, "Current data: null")
+            }
+        }
+    }
     fun onFrakcjaSelected(frakcja: Frakcja, onNavigate: (String) -> Unit) {
         onNavigate("game_screen/${frakcja.name}")
     }
